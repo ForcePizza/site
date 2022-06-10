2fcb2b3c125926560802868a88c313e4c0790124
ForcePizza <64161802+ForcePizza@users.noreply.github.com>
2022-06-10 12:43:52 +0300

    Add files via upload

---
+++ precache-manifest.d99f73f88fd86744d0f92bc0aab0a9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bd12c48f89163ac48733d961d7aff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716ba0a5033a4e772b80",
+    "url": "/static/css/main.a505383a.chunk.css"
+  },
+  {
+    "revision": "dcc9376e36fc6d6e6ec7",
+    "url": "/static/js/2.e9f3b57b.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.e9f3b57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716ba0a5033a4e772b80",
+    "url": "/static/js/main.d314558e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
